8147e81c6e7ff95d67b7835d2de1a3d671ecbf49
Aritra Das <97398407+AritraDasIT@users.noreply.github.com>
2024-01-29 09:51:41 +0530

    Create String_Compare.java

---
+++ String_Compare.java
@@ -0,0 +1,19 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    
+	    String name="ARItra";
+	    String name1="ARitra";
+	    
+	    if(name.compareTo(name1)==0)
+	    {
+	        System.out.println("Equal");
+	    }
+	    else
+	    {
+	        System.out.println("Not");
+	    }
+	}
+}
